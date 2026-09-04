38a1c125770032f4c1553e8cd9ea3c5835e1c6df
IcedPeppermintTea <238953108+IcedPeppermintTea@users.noreply.github.com>
2026-09-04 00:01:38 -0700

    WIP: search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="app">
       <h1>Weather</h1>
+      <Search></Search>
     </div>
   );
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,10 @@
+function Search() {
+  return (
+    <div className="search">
+      <div className="icon-gray-circle" aria-hidden="true"></div>
+      <input type="text" placeholder="search city" id="search"></input>
+    </div>
+  );
+}
+
+export default Search;
